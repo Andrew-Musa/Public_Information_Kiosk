37644340e9d062b91d68cae5da7527d2942e878a
Haofan Zhao <haofan.zhao@ucalgary.ca>
2024-03-03 23:03:41 -0700

    Finish Screen 7

--- public_information_kiosk/src/pages/Screen7.jsx
+++ public_information_kiosk/src/pages/Screen7.jsx
@@ -4,18 +4,19 @@ import Slider from '../components/slider';
 import SearchBar from '../components/searchBar';
 import Chinese from '../img/Chinese.png';
 import Pizza from '../img/Pizza.png';
-import Entertainments from '../img/entertaments.png';
-import Accommodations from '../img/Accomondations.png';
-import Transportations from '../img/Transportation.jpeg';
-import shopping from '../img/shopping.png';
-import cuisines from '../img/cuisines.png';
-import museum from '../img/museum.png';
+import Japanese from '../img/Japanese.png';
+import Indian from '../img/Indian.png';
+import Maxican from '../img/Maxican.jpeg';
+import Thai from '../img/Thai.png';
+import French from '../img/French.png';
+import Greek from '../img/Greek.png';
 
-export default function Screen5() {
+
+export default function Screen7() {
     const imageDataGroup1 = [
-        [{ image: Chinese, alt: 'Image 1_1', text: 'Chinese' }, { image: Pizza, alt: 'Image 1_2', text: 'Italian' }, { image: Entertainments, alt: 'Ima', text: 'Flight Information' }, { image: Accommodations, alt: 'Image 1_4', text: 'Housing' }],
-        [{ image: Transportations, alt: 'Image 1_5', text: 'Transit' }, { image: shopping, alt: 'Image 1_6', text: 'Shopping' },
-        {image: cuisines, alt: 'Image 1_3', text: 'Cuisines' }, { image: museum, alt: 'Image 1_4', text: 'Museum' }]
+        [{ image: Chinese, alt: 'Image 1_1', text: 'Chinese' }, { image: Pizza, alt: 'Image 1_2', text: 'Italian' }, { image: Japanese, alt: 'Ima', text: 'Japanese' }, { image: Indian, alt: 'Image 1_4', text: 'Indian' }],
+        [{ image: Maxican, alt: 'Image 1_5', text: 'Maxican' }, { image: Thai, alt: 'Image 1_6', text: 'Thai' },
+        {image: French, alt: 'Image 1_3', text: 'French' }, { image: Greek, alt: 'Image 1_4', text: 'Greek' }]
       ];
 
       const imageDataGroup2 = []
