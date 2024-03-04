0269dd0252cd35c2e6ee9885b0ecce2fb9eb4641
njungg <68088497+njungg@users.noreply.github.com>
2024-03-03 19:37:07 -0700

    added rough accessibility pages
    Created rough pages of accessibility landing page and other pages from there

--- public_information_kiosk/src/index.js
+++ public_information_kiosk/src/index.js
@@ -9,13 +9,40 @@ import Header from './components/header';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import './App.css'
-
+/////
+import AccessInfoLanding from './pages/accessInfo';
+import AccessInfo1 from './pages/accessInfo1';
+import AccessInfo2 from './pages/accessInfo2';
+import AccessInfo3 from './pages/accessInfo3';
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Landing />
   },
+  {
+    path: "/accessInfoLanding",
+    element: <AccessInfoLanding />
+  },
+  {
+
+
+    path: "/accessInfo1",
+    element: <AccessInfo1 />
+  },
+  {
+
+
+    path: "/accessInfo2",
+    element: <AccessInfo2 />
+  },
+  {
+
+
+    path: "/accessInfo3",
+    element: <AccessInfo3 />
+  }
 ]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -24,6 +51,7 @@ root.render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
