11619a6e17f2e459b674059c233bcd744f867619
Junyi Li <lyne8278@gmail.com>
2024-03-04 21:42:40 -0700

    chore: fix errors

--- public_information_kiosk/src/pages/insideNavigation.jsx
+++ public_information_kiosk/src/pages/insideNavigation.jsx
@@ -10,21 +10,12 @@ export default function InsideNavigation() {
                 <button className='btn btn-outline-dark'>Call a Volunteer<SupportAgent /></button>
             </div>
 
-<<<<<<< HEAD
             <div className="row px-5 mt-5">
                 <div className="col-10">
                     <small style={{ fontSize: "10px" }}><b>Open: </b>8:00-17:00 <b>Phone Number: </b>(888)888-8888</small>
                     <div style={{ height: "200px" }} className='border d-flex'>Map</div>
                 </div>
                 <div className="col-2 d-flex flex-column">
-=======
-            <div class="row px-5 mt-5">
-                <div class="col-10">
-                    <small style={{ fontSize: "10px" }}><b>Open: </b>8:00-17:00 <b>Phone Number: </b>(888)888-8888</small>
-                    <div style={{ height: "200px" }} className='border d-flex'>Map</div>
-                </div>
-                <div class="col-2 d-flex flex-column">
->>>>>>> main
                     <button className='btn mt-5 p-1'>
                         <ZoomIn />
                     </button>
@@ -41,13 +32,7 @@ export default function InsideNavigation() {
             </div>
             <div className='d-flex justify-content-center pt-5'>
                 <button className='btn btn-outline-dark'>Send Directions to My Phone</button>
-<<<<<<< HEAD
             </div>
-=======
-
-            </div>
-
->>>>>>> main
         </div >
     )
 }
