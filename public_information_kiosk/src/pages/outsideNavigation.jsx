855703c8ae390b7f845d7bfa5b1c5503e8ac7524
Andrew Musa <andrew.appd@gmail.com>
2024-04-03 20:23:33 -0600

    Added question for calling volunteer
    Added question for calling volunteer

--- public_information_kiosk/src/pages/outsideNavigation.jsx
+++ public_information_kiosk/src/pages/outsideNavigation.jsx
@@ -9,12 +9,19 @@ export default function OutsideNavigation() {
     const [showPopup, setShowPopup] = useState(false);
     const [accessibleMode, _] = useContext(Context);
 
+    const [showPopupText, setShowPopupText] = useState(false);
+
+    const handleYesCall = () => {
+        setShowPopupText(true);
+    };
+
     const handleClickVolunteer = () => {
         setShowPopup(true);
     };
 
     const handleClosePopup = () => {
         setShowPopup(false);
+        setShowPopupText(false);
     };
 
     const [showPopup2, setShowPopup2] = useState(false);
@@ -39,8 +46,12 @@ export default function OutsideNavigation() {
                 <>
                     <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)', zIndex: '9998' }} onClick={handleClosePopup}></div>
                     <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', background: 'white', padding: '20px', borderRadius: '5px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)', zIndex: '9999' }}>
-                        <button style={{ position: 'absolute', top: '10px', right: '10px', background: 'none', border: 'none', cursor: 'pointer', color: '#B82B35' }} onClick={handleClosePopup}>X</button>
-                        <p style={{ marginTop: '15px' }}>A white-hat volunteer is on their way to give assistance.</p>
+                        {showPopupText && <button style={{ position: 'absolute', top: '10px', right: '10px', background: 'none', border: 'none', cursor: 'pointer', color: '#B82B35' }} onClick={handleClosePopup}>X</button>}
+                        <p style={{ marginTop: '15px' }}>{!showPopupText ? "Are you sure you want to call a volunteer to your kiosk?" : "A white-hat volunteer is on their way to assist you. The volunteer will be there in approximately 2 minutes."}</p>
+                        <div className='d-flex justify-content-center'>
+                            {!showPopupText && <button className='backButton' style={{marginRight: 5}} onClick={handleClosePopup}>No, Don't</button>}
+                            {!showPopupText && <button className='backButton' onClick={handleYesCall}>Yes, Call</button>}
+                        </div>
                     </div>
                 </>
             )}
